cbdfa63b3385394cfa7503a40a214c4c2be94a6d
Guillaume Forestier <gforestier2000@yahoo.fr>
2021-11-03 14:41:53 +0100

    inialisation de la base dans le container de test

--- schemadb.sql
+++ schemadb.sql
@@ -4,22 +4,6 @@
 -- ------------------------------------------------------
 -- Server version       8.0.26
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `chess`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `chess` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `chess`;
 
@@ -28,8 +12,7 @@ USE `chess`;
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'id de l''utilisateur',
   `email` varchar(250) NOT NULL COMMENT 'email de l''utilisateur',
@@ -37,16 +20,14 @@ CREATE TABLE `users` (
   `lastname` varchar(250) DEFAULT NULL COMMENT 'nom de l''utilisateur',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4  COMMENT='Utilisateurs de l''application';
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=1 COMMENT='Utilisateurs de l''application';
 
 --
 -- Table structure for table `chessgame`
 --
 
 DROP TABLE IF EXISTS `chessgame`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `chessgame` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID de la partie',
   `whiteplayerid` int DEFAULT NULL COMMENT 'ID du player qui joue avec les blancs',
@@ -90,16 +71,5 @@ CREATE TABLE `chessgame` (
   KEY `blackplayer_idx` (`blackplayerid`),
   CONSTRAINT `blackplayer` FOREIGN KEY (`blackplayerid`) REFERENCES `users` (`id`),
   CONSTRAINT `whiteplayer` FOREIGN KEY (`whiteplayerid`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COMMENT='Table enregistrant les joueurs d''une partie et la situation courante des pi├¿ces';
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=1 COMMENT='Table enregistrant les joueurs d''une partie et la situation courante des pi├¿ces';
 
-
-CREATE USER 'appchess'@'%' IDENTIFIED VIA mysql_native_password USING '***';
-GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'appchess'@'%' REQUIRE NONE WITH 
-MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-
-CREATE USER 'appchess1'@'%' IDENTIFIED VIA mysql_native_password USING '***';
-GRANT USAGE ON *.* TO 'appchess1'@'%' REQUIRE NONE WITH 
-MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-GRANT ALL PRIVILEGES ON `gfo`.* TO 'appchess1'@'%'
--- Dump completed on 2021-10-21 15:34:25
