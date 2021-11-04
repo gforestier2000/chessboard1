0d4a0b7dcaa3986ce8da40060b6935cef99a6710
Guillaume Forestier <gforestier2000@yahoo.fr>
2021-11-05 00:35:07 +0100

    update CI script 8

--- model/chessgame.js
+++ model/chessgame.js
@@ -20,6 +20,67 @@ const mysqlConnection = MySQL.createConnection({
     database: process.env.DB_DATABASE
 });
 
+
+function  checkDatabase(){
+    const tableExist = "CREATE TABLE `chessgame` ( \
+        `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de la partie', \
+        `whiteplayerid` int(11) DEFAULT NULL COMMENT 'ID du player qui joue avec les blancs', \
+        `blackplayerid` int(11) DEFAULT NULL COMMENT 'ID du joueur qui joue avec les noirs', \
+        `name` varchar(45) DEFAULT NULL COMMENT 'Nom de la partie', \
+        `playerturn` varchar(1) NOT NULL DEFAULT 'W' COMMENT 'identifiant de la couleurqui doit jouer : W or B', \
+        `a1` varchar(2) DEFAULT 'TB' COMMENT 'case a1 du jeu d''echec', \
+        `b1` varchar(2) DEFAULT 'CB' COMMENT 'case b1 du jeu d''echec', \
+        `c1` varchar(2) DEFAULT 'FB' COMMENT 'Case c1 du jeu d''echec', \
+        `d1` varchar(2) DEFAULT 'DB' COMMENT 'Case d1 du jeu d''echec', \
+        `e1` varchar(2) DEFAULT 'RB' COMMENT 'Case e1 du jeu d''echec', \
+        `f1` varchar(2) DEFAULT 'FB' COMMENT 'Case f1 du jeu d''echec', \
+        `g1` varchar(2) DEFAULT 'CB' COMMENT 'Case g1 du jeu d''echec', \
+        `h1` varchar(2) DEFAULT 'TB' COMMENT 'Case h1 du jeu d''echec', \
+        `a2` varchar(2) DEFAULT 'PB' COMMENT 'case a2 du jeu d''echec', \
+        `b2` varchar(2) DEFAULT 'PB' COMMENT 'case b2 du jeu d''echec', \
+        `c2` varchar(2) DEFAULT 'PB' COMMENT 'Case c2 du jeu d''echec', \
+        `d2` varchar(2) DEFAULT 'PB' COMMENT 'Case d2 du jeu d''echec', \
+        `e2` varchar(2) DEFAULT 'PB' COMMENT 'Case e2 du jeu d''echec', \
+        `f2` varchar(2) DEFAULT 'PB' COMMENT 'Case f2 du jeu d''echec', \
+        `g2` varchar(2) DEFAULT 'PB' COMMENT 'Case g2 du jeu d''echec', \
+        `h2` varchar(2) DEFAULT 'PB' COMMENT 'Case h2 du jeu d''echec', \
+        `a3` varchar(2) DEFAULT '' COMMENT 'case a3 du jeu d''echec', \
+        `b3` varchar(2) DEFAULT '' COMMENT 'case b3 du jeu d''echec', \
+        `c3` varchar(2) DEFAULT '' COMMENT 'Case c3 du jeu d''echec', \
+        `d3` varchar(2) DEFAULT '' COMMENT 'Case d3 du jeu d''echec', \
+        `e3` varchar(2) DEFAULT '' COMMENT 'Case e3 du jeu d''echec', \
+        `f3` varchar(2) DEFAULT '' COMMENT 'Case f3 du jeu d''echec', \
+        `g3` varchar(2) DEFAULT '' COMMENT 'Case g3 du jeu d''echec', \
+        `h3` varchar(2) DEFAULT '' COMMENT 'Case h3 du jeu d''echec', \
+        `a4` varchar(2) DEFAULT '' COMMENT 'case a4 du jeu d''echec', \
+        `b4` varchar(2) DEFAULT '' COMMENT 'case b4 du jeu d''echec', \
+        `c4` varchar(2) DEFAULT '' COMMENT 'Case c4 du jeu d''echec', \
+        `d4` varchar(2) DEFAULT '' COMMENT 'Case d4 du jeu d''echec', \
+        `e4` varchar(2) DEFAULT '' COMMENT 'Case e4 du jeu d''echec', \
+        `f4` varchar(2) DEFAULT '' COMMENT 'Case f4 du jeu d''echec', \
+        `g4` varchar(2) DEFAULT '' COMMENT 'Case g4 du jeu d''echec', \
+        `h4` varchar(2) DEFAULT '' COMMENT 'Case h4 du jeu d''echec', \
+        PRIMARY KEY (`id`) , \
+        KEY `whiteplayer_idx` (`whiteplayerid`), \
+        KEY `blackplayer_idx` (`blackplayerid`), \
+        CONSTRAINT `blackplayer` FOREIGN KEY (`blackplayerid`) REFERENCES `users` (`id`), \
+        CONSTRAINT `whiteplayer` FOREIGN KEY (`whiteplayerid`) REFERENCES `users` (`id`) \
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='Table enregistrant les joueurs d''une partie et la situation courante des pièces';"
+    mysqlConnection.query(tableExist, (err, res) => {
+        if (err) {
+            //logger.debug("DB INSERT failed");
+            logger.debug(err.message);
+            return false;
+        }
+        //logger.debug("DB INSERT succeed");
+        logger.debug(res);
+        return true;
+    });
+}
+
+checkDatabase();
+
+
 process.on('beforeExit',() => {
     logger.fatal("beforeExit");
 });
